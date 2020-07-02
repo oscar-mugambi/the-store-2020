2d8290d3d9eb8acf17f70c2a2463a4aabd2f696f
Oscar Mugambi <oscarmugambi@gmail.com>
2020-07-02 13:14:45 +0300

    header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 // const Homepage1 = (props) => {
@@ -31,8 +32,11 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
