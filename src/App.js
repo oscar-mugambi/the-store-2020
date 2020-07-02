d679c9e37e8d6cf205fd90a6e3a4373eb12bbf3e
Oscar Mugambi <oscarmugambi@gmail.com>
2020-07-02 12:33:15 +0300

    shop page, collection preview

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component.jsx";
 import "./App.css";
 
+// const Homepage1 = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// };
+// const TopicList = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>TOPIC List</h1>
+//     </div>
+//   );
+// };
+// const TopicDetail = () => {
+//   return (
+//     <div>
+//       <h1>TOPIC DETAIL</h1>
+//     </div>
+//   );
+// };
+
 function App() {
-  return;
-  <div>
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-    </Switch>
-  </div>;
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-function MenuItem({ title, imageUrl, size, history, linkUrl,match }) {
+function MenuItem({ title, imageUrl, size, history, linkUrl, match }) {
   return (
-    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url.} ${linkUrl}`)}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url} ${linkUrl}`)}
+    >
       <div
         style={{
           backgroundImage: `url(${imageUrl})`,
